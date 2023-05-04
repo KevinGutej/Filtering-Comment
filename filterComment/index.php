e69bd00002c3322638a30e214053d7c4e32823ec
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-05-04 22:28:30 +0200

    Filtering the comment

--- filterComment/index.php
+++ filterComment/index.php
@@ -10,5 +10,19 @@
 
 
 </body>
+<?php
+
+if (!empty($_GET["name"]) && isset($_GET["name"])) {
+  echo "Succesful" . "<br>";
+  if (!empty($_GET["comment"]) && isset($_GET["comment"])) {
+    echo "Your name is: " . $_GET["name"] . "<br>";
+    echo "Your comment before filter is: " . $_GET["comment"] . "<br>";
+    echo "You comment without html tags: " . strip_tags($_GET["comment"])  . "<br>";
+    $userComment = $_GET["comment"];
+    $filteredComment = htmlspecialchars($userComment);
+    echo "Your comment after filter is: " . $filteredComment;
+  }
+}
+?>
 
 </html>
