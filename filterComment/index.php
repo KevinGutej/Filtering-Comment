80d79d2b6fc6497038126725b7e0427a8e18def4
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-05-04 22:27:51 +0200

    Creating form

---
+++ filterComment/index.php
@@ -0,0 +1,14 @@
+<html>
+
+<body>
+  <form action="index.php" method="GET">
+    <input type="text" name="name" placeholder="name">
+    <input type="text" name="comment" placeholder="comment">
+    <input type="submit" value="Post comment">
+  </form>
+
+
+
+</body>
+
+</html>
